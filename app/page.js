81af2a0b169a4c07b3948c1531c349d51facd092
AddE <40864924+Addechander@users.noreply.github.com>
2024-08-05 21:20:16 +0545

    Update page.js

--- app/page.js
+++ app/page.js
@@ -105,7 +105,7 @@ const updateQuantity = async (id, newQuantity) => {
 };
   return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm ">
+        <div className="z-10 max-w-8xl w-full items-center justify-between font-mono text-sm ">
           <h1 className='text-4xl p-4 text-center'>Pantry Tracker</h1>
           <div className="bg-slate-900 p-10 rounded-lg">
             <form className="grid grid-cols-6 items-center text-black">
