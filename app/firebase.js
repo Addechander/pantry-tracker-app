d77c1c62f30ed05d8950ceb4fde017d7d8a35e97
AddE <40864924+Addechander@users.noreply.github.com>
2024-08-05 21:21:32 +0545

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 
-
+//API
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
